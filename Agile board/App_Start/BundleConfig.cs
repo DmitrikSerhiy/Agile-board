2b06694df274d055c4212f161a9ac8bc16af2b89
Сергей Дмитрик <Dr.voo1646@gmail.com>
2018-10-12 06:22:40 +0300

    deleted jQuery added jQuery SDN

--- Agile board/App_Start/BundleConfig.cs
+++ Agile board/App_Start/BundleConfig.cs
@@ -8,8 +8,6 @@ namespace Agile_board
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
